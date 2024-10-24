e4580a3243734680f069f3177e50cd3e5eaebc3b
Alex <melnykoleksandr547@gmail.com>
2024-10-24 23:07:06 +0200

    Firs day of learning

---
+++ server/routes/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('', (req, res) => {
+    const locals = {
+        title: "Node.js blog",
+        description: "Simple Blog created with Node.js, Express & MongoDB"
+    }
+
+    res.render('index',{ locals });
+
+})
+router.get('/about', (req, res) => {
+    res.render('about');
+
+})
+module.exports = router;
